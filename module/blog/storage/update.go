47241f3e852e0f3ea36060fff799ab1c1d53d136
vktrung <86977539+vktrung@users.noreply.github.com>
2025-10-10 22:08:28 +0700

    Update update.go

--- module/blog/storage/update.go
+++ module/blog/storage/update.go
@@ -7,8 +7,28 @@ import (
 )
 
 func (s *sqlStore) UpdateBlog(ctx context.Context, id int, data *model.BlogUpdate) error {
-	if err := s.db.Table(model.Blog{}.TableName()).Where("id = ?", id).Updates(data).Error; err != nil {
-		return common.ErrDB(err)
+	// Tạo map để chỉ update những trường có giá trị (không phải nil)
+	updates := make(map[string]interface{})
+	
+	if data.Title != nil {
+		updates["title"] = *data.Title
 	}
+	if data.Content != nil {
+		updates["content"] = *data.Content
+	}
+	if data.Status != nil {
+		updates["status"] = *data.Status
+	}
+	if data.Image != nil {
+		updates["image"] = data.Image
+	}
+	
+	// Chỉ update nếu có ít nhất một trường cần update
+	if len(updates) > 0 {
+		if err := s.db.Table(model.Blog{}.TableName()).Where("id = ?", id).Updates(updates).Error; err != nil {
+			return common.ErrDB(err)
+		}
+	}
+	
 	return nil
 }
